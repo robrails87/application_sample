d13f11186ae08f0fde52c14d928337cedcf08476
Rob Pliszka <robrails87@gmail.com>
2012-10-18 01:23:19 -0400

    All pages-controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,10 @@ class PagesController < ApplicationController
 
   def contact
   end
+  
+  def about
+  end
+  
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ ApplicationSample::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
+  
+  get "pages/help"
 
 
 end
